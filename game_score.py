9a568dc8f9d1f611a9c6186890d8c69e6d0f7682
Kosmi <vukasin.jevtic@gmail.com>
2024-05-26 22:15:33 +0200

    Task_1

---
+++ game_score.py
@@ -0,0 +1,85 @@
+# Task 1. Develop a function to determine the score in a game 
+
+# Objective 
+# In the code example below, a list of game score states throughout a match is generated.
+# Develop the function get_score(game_stamps, offset), which will return the score at the moment of offset in the list of game_stamps.
+# It is necessary to understand the essence of the written code, notice the nuances, develop a function that fits in style with the existing code, 
+# preferably with adequate algorithmic complexity.
+
+
+from pprint import pprint
+import random
+import math
+
+TIMESTAMPS_COUNT = 50000
+
+PROBABILITY_SCORE_CHANGED = 0.0001
+
+PROBABILITY_HOME_SCORE = 0.45
+
+OFFSET_MAX_STEP = 3
+
+INITIAL_STAMP = {
+    "offset": 0,
+    "score": {
+        "home": 0,
+        "away": 0
+    }
+}
+
+
+def generate_stamp(previous_value):
+    score_changed = random.random() > 1 - PROBABILITY_SCORE_CHANGED
+    home_score_change = 1 if score_changed and random.random() > 1 - \
+        PROBABILITY_HOME_SCORE else 0
+    away_score_change = 1 if score_changed and not home_score_change else 0
+    offset_change = math.floor(random.random() * OFFSET_MAX_STEP) + 1
+
+    return {
+        "offset": previous_value["offset"] + offset_change,
+        "score": {
+            "home": previous_value["score"]["home"] + home_score_change,
+            "away": previous_value["score"]["away"] + away_score_change
+        }
+    }
+
+
+def generate_game():
+    stamps = [INITIAL_STAMP, ]
+    current_stamp = INITIAL_STAMP
+    for _ in range(TIMESTAMPS_COUNT):
+        current_stamp = generate_stamp(current_stamp)
+        stamps.append(current_stamp)
+
+    return stamps
+
+
+game_stamps = generate_game()
+
+pprint(game_stamps)
+
+
+def get_score(game_stamps, offset):
+    '''
+        Takes list of game's stamps and time offset for which returns the scores for the home and away teams.
+        Please pay attention to that for some offsets the game_stamps list may not contain scores.
+    '''
+    # return home, away
+
+    if offset < 0:
+        raise ValueError("Offset must be a non-negative integer")
+
+    start = 0
+    end = len(game_stamps) - 1
+
+    while (start <= end):
+        mid = (start + end) // 2
+
+        if game_stamps[mid]['offset'] == offset:
+            return game_stamps[mid]['score']['home'], game_stamps[mid]['score']['away']
+        elif game_stamps[mid]['offset'] > offset:
+            end = mid - 1
+        else:
+            start = mid + 1
+        
+    return game_stamps[end]['score']['home'], game_stamps[end]['score']['away']
